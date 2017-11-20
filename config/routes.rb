aa4d1a80ecae77a235f0ae05668df0ea231184f7
Situ William <situ_william@yahoo.com>
2017-11-20 14:49:06 -0500

    added a model of pictures to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+	# /pictures will go to the pictures controller index method
+	# Math any http GET request for the URL /pictures to the index metod in Pictures COntroller
+	# GET http Request
+	# 'Pictures' specifies URL 
+	# =>, # syntax for defining routes
+	# 'pictures name of controller, the class called PicturesController
+	# index Name of method in controller that gets executed when someone makes request
 	get 'pictures' => 'pictures#index'
 end
